e44c253807ce14c73154e0b90f43e96a1ca27007
BiBa-01 <birtebalzer@web.de>
2023-06-08 18:48:19 +0200

    change admin user#

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   admin_username      = "${var.vm_admin_username}"
   network_interface_ids = []
   admin_ssh_key {
-    username = "adminuser"
+    username = "odl_user_235293@udacityhol.onmicrosoft.com"
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDXg4XFyaZElYVo0mljBKi33xS9RPDqYiehibpex6l3kYZlxspVt3eCkucw0lLSOaL2mx37Z3p9n7na+94uev5MwfxqALLTKSRXQQnNgyj4GJGEFisXR3xOElT/TsLQmuEBR7EtN1Y4ho8ODBs2pcH+MLt26DC1+EXC8LtJ6KxgLF//brBOkHhWqJIuuI20Zyo30hnNhLMSqOjOWi2SyuBJyZqaIlStFsld9RmPmvLFzX56yr1JRbvZyBcBTqcdorQcdBCSFtTYavPKBKye3Hi9UAyJYqcZElcsmZqxz3vEPw/jMRBZG4slV7Eu8zW8sN6tpmf0fQFki83ZD5V5A7IV8XFFG+tc+mSb25cWyMwNdMi/mpbENVu3PYumeoCChBedIdF5mFN/TmNb11VO4YLFgsUjLAAROTSiGT//NkBkpIyueX/TFF0= odl_user@cc-f958-ab119495-6448dd6dbb-pcbfh" 
 }
 
