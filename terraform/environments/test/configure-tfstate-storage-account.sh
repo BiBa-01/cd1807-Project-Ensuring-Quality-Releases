94d4a9bf4da3224ee88099a6fef611af786c27e1
BiBa-01 <birtebalzer@web.de>
2023-06-08 17:04:57 +0200

    rename tfstate in tfsate1#

--- terraform/environments/test/configure-tfstate-storage-account.sh
+++ terraform/environments/test/configure-tfstate-storage-account.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RESOURCE_GROUP_NAME="Azuredevops"
-STORAGE_ACCOUNT_NAME="tfstate$Random$Random"
-CONTAINER_NAME="tfstate"
+STORAGE_ACCOUNT_NAME="tfstate1$Random$Random"
+CONTAINER_NAME="tfstate1"
 
 # This command is not needed in the Udacity provided Azure account. 
 # Create resource group
