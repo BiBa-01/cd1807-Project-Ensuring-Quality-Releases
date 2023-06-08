559c6145a7b6c2125bf3b19a646f077d1c75c91b
BiBa-01 <birtebalzer@web.de>
2023-06-08 18:04:27 +0200

    adding information to vm.tf#

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,22 +1,23 @@
 resource "azurerm_network_interface" "azuredevops_interface" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+  name                = "${var.application_type}-${var.resource_type}-nic"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = "${var.public_ip_address_id}"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "test" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
   size                = "Standard_DS2_v2"
-  admin_username      = ""
+  admin_username      = "${var.vm_admin_username}"
   network_interface_ids = []
   admin_ssh_key {
     username = "adminuser"
