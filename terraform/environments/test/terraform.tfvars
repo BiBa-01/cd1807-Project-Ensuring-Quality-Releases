c23a4f09f79f9880174caf5b2646194031edb191
BiBa-01 <birtebalzer@web.de>
2023-06-10 20:28:47 +0200

    update#

--- terraform/environments/test/terraform.tfvars
+++ terraform/environments/test/terraform.tfvars
@@ -1,8 +1,8 @@
 # Azure subscription vars
-subscription_id = "46f0b4f7-e5d8-488c-b438-00f74a74c84a"
-client_id = "6f99eb0f-ce98-4131-87bd-7d8a7a65991e"
-client_secret = "GTm8Q~rLvALs8rSy7sWLR4mA~dMA.9la.wLDsbIb"
-tenant_id = "f958e84a-92b8-439f-a62d-4f45996b6d07"
+subscription_id = "4e780871-9657-43bf-b521-9c73706b76b1"
+client_id = "217053e3-f5eb-4345-96d7-eb87f3883073"
+client_secret = "NTW8Q~NXA1QpnYrNR~FHWSTcuWZdUB4rmEAR3a6M"
+tenant_id = "4e780871-9657-43bf-b521-9c73706b76b1"
 
 # Resource Group/Location
 location = "East US"
@@ -10,7 +10,7 @@ resource_group_name = "AzureDevops"
 application_type = "myApplication"
 
 # Network
-virtual_network_name = "vnetproject"
+virtual_network_name = ""
 address_space = ["10.5.0.0/16"]
 address_prefix_test = "10.5.1.0/24"
 
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,8 +20,8 @@ resource "azurerm_linux_virtual_machine" "test" {
   admin_username      = "${var.vm_admin_username}"
   network_interface_ids = [azurerm_network_interface.azuredevops_interface.id]
   admin_ssh_key {
-    username = "odl_user_235440@udacityhol.onmicrosoft.com"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDh/v2+VUOdqd2NvqAU/VYrw1ZSs9iZ5iE6oHyE5JaOpAlT+KRyfHNAlcOELl3ow7sj68ILFuF4yD31tXqxPr4mkv8TDbVpNEsMJYbxZfZG6kWf3re9boOSadHJRS4jqFRCf8YJm2DBeBSKQkFq4AD67B/J2wWl/2q/svL1qc8jX2knYHye/BiTOH8VLeEP1Nnkl5VZrSyTMuY5hZ/hXSB1u03sbTq60EFLmrDmkcOrrhqBIR2V2t5rC2krVGNZeTGtGZk/qz1zfP5Z2q/5DgylsFUUznXy8eg+ugsSMFGeakjWzxsz9nR01e/1zFM9MYnkr65Pl44U+HT0b6+RLyauwr49zHGvhetKQheho64Ute4hN4t5E0DtBAH1arnjLylR4GdB7XmIavJIkVj8zrDP7bJhfNx18Mth/Gyfz6H8S6YzaFG4HahPa3ZcdOMbg9L5ZoxQBtlKiKkWiM0qk/zf5B5DKk+wAdSXwRgjwV89/IXkNfBcK0fonrgWsn5g2Is= odl_user@cc-1f4a9e36-65cc7d7c7d-gs8mh"
+    username = "odl_user_235472@udacityhol.onmicrosoft.com"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDDxv5dqUNKdx3MkUUxWQz1YCxUHZU+pKZve1OA2eLsrm++B6MFoZV9GBKEi8RFIWA5uoHIGE7aox6LMYOImQFVKXy0zT/7PPv6xhm9I0A/nvz43zdfCvMYaMuxJ8t6ZrD3vlxHdq3Uei+tgVyyy7HQcw8J0jy8S/G1chgzmgxLC+WGCSTqwWtrPWCnbgmo2o46dRbSB6AHomNXX7IIf39pKJ4vZabcGuGWbCl7X8B9HvlbC0XkA9QhHtJcP3Aj6rpOhLD6fMU7FDp456IajLSCGIPo5/SYzSq3HI7S5V3sDtp1VrSzVlObkaYOh7RG7c20C2wNwH5BDXBFNYgUOHoCujqVAiRumIJMpzH4+2J8N7CKGkUdwbVP0HiVrpAVx72bJD1KQsFOud8hOlF5vZ7AuRh0ra5AhS4vZ+Wq57sTrB0PdHmt+7cJlawMiKjvShInEYbVRM8k0KluT4DHgSq2W4DpK9XmeT2e9ZTekMGy078JaXwwcH5OsN7y6wJO6Gc= odl_user@cc-7c05eda8-6d7464d4b4-lwlkl"
     }
 
   os_disk {
