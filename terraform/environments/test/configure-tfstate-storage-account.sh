6be85d2d5197214aefdc57eee83f180cb0d6a5b8
BiBa-01 <birtebalzer@web.de>
2023-06-12 22:50:52 +0200

    update#

--- terraform/environments/test/configure-tfstate-storage-account.sh
+++ terraform/environments/test/configure-tfstate-storage-account.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 RESOURCE_GROUP_NAME="Azuredevops"
-STORAGE_ACCOUNT_NAME="tfstatebb5"
-CONTAINER_NAME="tfstatebb5"
+STORAGE_ACCOUNT_NAME="tfstatebb6"
+CONTAINER_NAME="tfstatebb6"
 
 # This command is not needed in the Udacity provided Azure account. 
 # Create resource group
 # az group create --name $RESOURCE_GROUP_NAME --location eastus
 
 # Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob --public-access blob
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob 
 
 # Get storage account key
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
-export ARM_ACCESS_KEY=$ACCESS_KE
+export ARM_ACCESS_KEY=$ACCESS_KEY
 
 
 # Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY --public-access blob
 echo "RESOURCE_GROUP_NAME=$RESOURCE_GROUP_NAME"
 echo "STORAGE_ACCOUNT_NAME=$STORAGE_ACCOUNT_NAME"
 echo "CONTAINER_NAME=$CONTAINER_NAME"
 echo "ACCOUNT_KEY=$ACCOUNT_KEY"
-echo "ACCESS_KEY=$ACCESS_KEY"
-echo $ARM_ACCESS_KEY
+echo "ARM_ACCESS_KEY=$ARM_ACCESS_KEY"
+
+
+
+
+
+
+
+
+
+
 
 
