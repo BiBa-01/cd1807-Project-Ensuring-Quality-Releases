1f1a8df95bb2778d7e5199ee10a8a72c40059301
BiBa-01 <birtebalzer@web.de>
2023-06-12 23:53:32 +0200

    update#

--- terraform/modules/resource_group/rg.tf
+++ terraform/modules/resource_group/rg.tf
@@ -2,5 +2,4 @@
 resource "azurerm_resource_group" "test" {
   name     = "${var.resource_group}"
   location = "${var.location}"
-  terraform import azurerm_resource_group.test /subscriptions/157081ad-2288-4aa4-b6d0-69f2165b7326/resourceGroups/Azuredevops
 }
