d947d43e98445c3ce146daa34f1c51a96080948e
BiBa-01 <birtebalzer@web.de>
2023-06-14 16:05:37 +0200

    update#

--- terraform/modules/resource_group/rg.tf
+++ terraform/modules/resource_group/rg.tf
@@ -1,7 +1,4 @@
-data "azurerm_resource_group" "test" {
-  name = "existing"
-}
-
-output "id" {
-  value = data.azurerm_resource_group.test.id
+resource "azurerm_resource_group" "test" {
+  name     = "${var.resource_group}"
+  location = "${var.location}"
 }
