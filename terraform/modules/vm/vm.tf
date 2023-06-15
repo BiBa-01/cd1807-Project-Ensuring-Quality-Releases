18bacd2294bba60a3af16b31a0b5f76b80dea5e0
BiBa-01 <birtebalzer@web.de>
2023-06-15 08:36:01 +0200

    update#

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_interface" "test1" {
+resource "azurerm_network_interface" "test" {
   name                = "${var.application_type}-${var.resource_type}-nic"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
@@ -12,13 +12,13 @@ resource "azurerm_network_interface" "test1" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "test1" {
+resource "azurerm_linux_virtual_machine" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   size                = "Standard_B1s"
   admin_username      = "${var.vm_admin_username}"
-  network_interface_ids = [azurerm_network_interface.test1.id]
+  network_interface_ids = [azurerm_network_interface.test.id]
 
   admin_ssh_key {
     username = "birtebalzer@gmail.com"
