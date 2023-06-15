48ced93b6cf2286aed7bf269d662b6e19d406dad
BiBa-01 <birtebalzer@web.de>
2023-06-15 11:28:34 +0200

    update#

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,11 +1,17 @@
-variable "location" {}
-variable "resource_group" {}
+variable "location" {
+    default = "eastus" }
+
+variable "resource_group" {
+    default = "project"}
 
 variable "public_ip_address_id" {}
 variable "subnet_id" {}
 
 
-variable "application_type" {}
-variable "resource_type" {}
+variable "application_type" {
+    default = "ProjectApp"}
+
+variable "resource_type" {
+    default = " project"}
 
 variable "vm_admin_username" {}
