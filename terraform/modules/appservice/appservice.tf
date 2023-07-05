5f2d320adcc6aa447b99dcbea7786922b2ab6c30
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-07-05 09:46:41 +0200

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -23,7 +23,6 @@ resource "azurerm_linux_web_app" "test" {
 
 resource "azurerm_service_plan" "test1" {
   name                = "${var.application_type}-${var.resource_type}"
-  app_service_plan_id = azurerm_app_service_plan.test1.id
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   os_type             = "Windows"
@@ -32,6 +31,7 @@ resource "azurerm_service_plan" "test1" {
 
 resource "azurerm_app_service" "test1" {
   name                = "${var.application_type}-${var.resource_type}"
+  app_service_plan_id = azurerm_app_service_plan.test1.id
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 
