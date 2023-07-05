11181daeea857caeb45755c16bf83b57ec1c8ae4
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-07-05 09:19:05 +0200

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -21,7 +21,7 @@ resource "azurerm_linux_web_app" "test" {
   }
 }
 
-resource "azurerm_service_plan" "test" {
+resource "azurerm_service_plan" "test1" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
@@ -29,7 +29,7 @@ resource "azurerm_service_plan" "test" {
   sku_name            = "F1"
 }
 
-resource "azurerm_app_service" "test" {
+resource "azurerm_app_service" "test1" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
