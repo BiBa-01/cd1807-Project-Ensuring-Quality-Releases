f74ab8ee19f3cd74ced2f0af771ffefab9970f32
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-07-10 07:25:21 +0200

    Update configure-tfstate-storage-account.sh

--- terraform/environments/test/configure-tfstate-storage-account.sh
+++ terraform/environments/test/configure-tfstate-storage-account.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-RESOURCE_GROUP_NAME="project"
-STORAGE_ACCOUNT_NAME="tfstatebb8"
-CONTAINER_NAME="tfstatebb8"
+RESOURCE_GROUP_NAME="Azuredevops"
+STORAGE_ACCOUNT_NAME=""
+CONTAINER_NAME=""
 
 # This command is not needed in the Udacity provided Azure account. 
 # Create resource group
