c2f6b5a29836f0bc1f8b4f83f5d8e97df5751a7f
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-07-10 13:26:44 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -11,10 +11,10 @@ terraform {
   }
 }
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.environmentServiceNameAzureRM
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
