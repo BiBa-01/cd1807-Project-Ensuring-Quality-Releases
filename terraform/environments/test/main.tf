9e19b609d04b79a3147f83095b203f289c3cedbc
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-07-10 12:24:30 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -33,7 +33,7 @@ terraform {
 
 
 module "resource_group" {
-#  source               = "../../modules/resource_group"
+  source               = "../../modules/resource_group"
   resource_group = data.azurerm_resource_group.existing.azuredevops
  # location             = "${var.location}"
 }
