2ff603222f1e2980ce7eafe0209a8cc1c784cc56
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-07-10 11:15:18 +0200

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   admin_username      =  var.vm_admin_username #"${var.vm_admin_username}"
   network_interface_ids = [azurerm_network_interface.test.id]
 
-  admin_ssh_key {
+  admin_ssh_key = {
   username = "adminuser"
   public_key = "AAAAB3NzaC1yc2EAAAADAQABAAABgQDfJRlaoTLF6zA7MEwhM72RwNjCw9B+ZaQTkDItOPabdE+dyy9Qp88tHu/A1PZwxcZejr814k0Aey8EE8PnvUc6CtvE99hUpk54Jqk7/y0FMVh9oSoQfKxl4t2YZuUhhMB1x9fAWzxunCUQzkCfTM9H7H0WiZF9Jj+bF1Y83oRFMIDaV4avdNH3giyYGbzWiGZwnFrbrVOdhQXRDKQh2QIdSr5j3VtZ2INSv2ytjLFw22rjqPh7MoEYkn6Ald6IxZqi0aCKRf/kZL4oIW9GWxVThk/9e+iqbTbZ4mNu6WVieyfhJ/SJFBWC4m7pHBAb9BHxDDZ0bpSAZK+rrOpLf8z99FE9R9WEvfJTJw982omAutLfO6JY9kwXeZ5Fg58oa2brv07zfIrZjWNbTl233b0b7utAHUDnh0oMOc6k1JgZwVjvtdULjxbhFNf6AVdSFezrcvBOyBposjYpXFouflxngNjSFwqQoyJCUPqEeqJg8EIWeu37Iw9jLqvD70y13fk= odl_user@cc-920f4460-565598d485-khldw" #file("~/.ssh/id_rsa.pub")
 }
