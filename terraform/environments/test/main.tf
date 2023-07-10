60ba5212121030be8e3318c647fb3938f8397432
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-07-10 11:55:11 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -28,11 +28,11 @@ terraform {
 #  #location             = "${var.location}"
 #}
 
-module "resource_group" {
-  source               = "../../modules/resource_group"
-  resource_group       = "${var.resource_group}"
-  location             = "${var.location}"
-}
+#module "resource_group" {
+ # source               = "../../modules/resource_group"
+  #resource_group       = "${var.resource_group}"
+  #location             = "${var.location}"
+#}
 module "network" {
   source               = "../../modules/network"
   address_space        = "${var.address_space}"
