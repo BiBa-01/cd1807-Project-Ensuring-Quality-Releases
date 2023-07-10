435ac40d0bf5f29942502a1fe0c2963efc853b64
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-07-10 12:00:19 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -20,19 +20,19 @@ terraform {
    }
  }
 
-resource "azurerm_resource_group" "test" {
-  name     = "Azuredevops"
-  location = "eastus"
+#resource "azurerm_resource_group" "test" {
+#  name     = "Azuredevops"
+#  location = "eastus"
   #source               = "../../modules/resource_group"
   #resource_group       = "${var.resource_group}"
   #location             = "${var.location}"
-}
-
-#module "resource_group" {
- # source               = "../../modules/resource_group"
-  #resource_group       = "${var.resource_group}"
-  #location             = "${var.location}"
 #}
+
+module "resource_group" {
+  source               = "../../modules/resource_group"
+  resource_group       = "${var.resource_group}"
+  location             = "${var.location}"
+}
 module "network" {
   source               = "../../modules/network"
   address_space        = "${var.address_space}"
